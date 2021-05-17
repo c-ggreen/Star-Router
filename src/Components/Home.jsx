5d3da0360b5dcd1287f755b23fab2a613ad1a791
Chadwick Green <chadggreen@gmail.com>
2021-05-16 23:30:23 -0400

    homepage change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,8 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>This is the Home page.</h1>
+                <h1>Welcome to the Home page.</h1>
+                <p>Please navigate to the Search Page to see website functionality.</p>
             </div>
         );
     }
